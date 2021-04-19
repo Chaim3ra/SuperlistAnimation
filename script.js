cee906733922ae60292d1f69eac17829993ac54e
Chaitanya Agarwal <chaitanya2205@gmail.com>
2021-04-19 19:59:03 +0530

    added pivot for cube and sphere

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let renderer;
 let camera;
 //let controls;
 
-
+/* Scene Creating*/
 let scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,0.1,1000),
 camera.position.set(0,2.5,3.75),
@@ -15,51 +15,64 @@ renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor("white");
+renderer.setClearColor("grey");
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-
+/* Initial Cube setup and creation */
 const cubeGeo = new THREE.BoxGeometry( 1, 1, 1 );
 const cubeMat = new THREE.MeshPhongMaterial({color: '#8AC'});
 const cube = new THREE.Mesh( cubeGeo, cubeMat );
 cube.position.set(0.5,0,0);
 scene.add( cube );
 
-
+/* Initial Sphere setup and creation */
 var sphereGeometry = new THREE.SphereGeometry(.1, 20, 20);
-        var matProps = {
+var matProps = {
 
-            specular: '#a9fcff',
-            color: '#00abb1',
-            emissive: '#006063',
-            shininess: 1
-        }
+    specular: '#a9fcff',
+    color: '#00abb1',
+    emissive: '#006063',
+    shininess: 1
+}
+var sphereMaterial = new THREE.MeshPhongMaterial(matProps);
+var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.castShadow = true;
+sphere.name = 'sphere';
+sphere.position.set(-0.5,0,0);
+scene.add(sphere);
 
-        var sphereMaterial = new THREE.MeshPhongMaterial(matProps);
-        var sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-        sphereMesh.castShadow = true;
-        sphereMesh.name = 'sphere';
-        sphereMesh.position.set(-0.5,0,0);
-        scene.add(sphereMesh);
-
+/* Light properties */
 const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(0, 1, 0);
-    light.target.position.set(0, 0, 0);
-    scene.add(light);
-    scene.add(light.target);
+const intensity = 0.65;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(0, 1.5, 3);
+light.target.position.set(0, 0, 0);
+scene.add(light);
+scene.add(light.target);
+
+// ambient
+scene.add( new THREE.AmbientLight( 0xffffff, 0.2 ) ); // optional
+
+
+
+const helper = new THREE.DirectionalLightHelper(light);
+scene.add(helper);
+
+pivot = new THREE.Group();
+pivot.position.set( 0.0, 0.0, 0 );
+scene.add( pivot );
+pivot.add( cube );
+pivot.add( sphere );
 
 
 function render() {
 renderer.render(scene, camera);
 }
 
+/* main */
 let animate = function() {
     requestAnimationFrame(animate);
-
-    //controls.update();
     renderer.render(scene, camera);
 };
 
@@ -71,47 +84,40 @@ function onWindowResize() {
     camera.aspect = container.clientWidth / container.clientHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(container.clientWidth, container.clientHeight);
-    }
+}
     
-    window.addEventListener("resize", onWindowResize);
+window.addEventListener("resize", onWindowResize);
     
-    
-    
-    
-    gsap.registerPlugin(ScrollTrigger);
+       
+gsap.registerPlugin(ScrollTrigger);
         
+           
+ScrollTrigger.defaults({
+    immediateRender: false,
+    ease: "power1.easeinOut",
+});
     
-        
-    ScrollTrigger.defaults({
-      immediateRender: false,
-      ease: "power1.easeinOut",
-    });
     
-    
-    let tl = gsap.timeline({
-    
-            scrollTrigger: {
-              trigger: ".section-one",
-              start: "top top", 
-              endTrigger: ".section-five",
-              end: "bottom bottom", 
-              scrub: 2, 
-           }
-          
-        });
+let tl = gsap.timeline({
+    scrollTrigger: {
+    trigger: ".section-one",
+    start: "top top", 
+    endTrigger: ".section-five",
+    end: "bottom bottom", 
+    scrub: 2, 
+          }          
+  });
     
 
-          tl.to(scene.rotation, { y: 1.2 ,duration:1.5},">1")
-          .to(sphereMesh.scale,{x:4.75,y:4.75,z:4.75,duration:1.5},"1")
-          .to(cube.position,{x:cube.position.x-0.05},"3")
-          .to(sphereMesh.position,{x:sphereMesh.position.x+0.05},"3")
-          .to(cube.position,{x:cube.position.x+0.05},"3.5")
-          .to(sphereMesh.position,{x:sphereMesh.position.x-0.05},"3.5")
-          .to(scene.rotation,{y:-3,duration:4},"4")
-          .to(cube.position,{x:0,duration:3},"4.6")
-          .to(sphereMesh.position,{x:0,duration:3},"4.6")
-          .to(cube.scale,{x:0,y:0,z:0,duration:3},"4.5")
-          .to(sphereMesh.scale,{x:0,y:0,z:0,duration:3},"4.5");
+tl.to(pivot.rotation, { y:3 ,duration:1.5},">1")
+.to(sphere.scale,{x:4.75,y:4.75,z:4.75,duration:1.5},"1")
+.to(cube.position,{x:cube.position.x-0.05},"3")
+.to(sphere.position,{x:sphere.position.x+0.05},"3")
+.to([cube.position,sphere],{x:cube.position.x+0.05},"3.5")
+.to(pivot.rotation,{y:-3.5,duration:3},"4")
+.to([cube.position,sphere.position],{x:0,duration:1.25},"4.6")
+.to([cube.scale,sphere.scale],{x:0,y:0,z:0,duration:1.25},"4.5");
+          
 
 
 
